37d4a362ab00fd8afa5853d24fdcbdc0cd6624e4
Angus W Ryer <angus@ryer.ca>
2020-07-20 11:52:15 -0400

    Initial commit

---
+++ src/config/google.config.js
@@ -0,0 +1,3 @@
+export const WEB_CLIENT_ID = '876062261559-0e83hodhtd4cc40hlr2bec9e86dg9om8.apps.googleusercontent.com'
+export const ANDROID_CLIENT_ID = '876062261559-k0lle98cb3q16e32fp7f5qp3pcuq7tpa.apps.googleusercontent.com'
+export const IOS_CLIENT_ID = '876062261559-g64ija73hluria4543da9vj3ermhkasp.apps.googleusercontent.com'
